b9575b282fc4fc769e40b6b36119f13bc5b1b6af
LeandroLoiacono <loiacono.leandro@gmail.com>
2025-06-13 04:45:26 +0200

    Dynamic hp

---
+++ frontend/src/static/js/fixtures/video-map.js
@@ -0,0 +1,33 @@
+export const PlaylistIds = {
+  it: 'z6l4XwVRZ',
+  en: 'l6ny9U9Nc',
+  fr: 'VcA9cZmA7',
+  de: 'GAdwW1mSD'
+}
+
+export const VideoIds = {
+  it: {
+    "sala-1-1": "", "sala-1-2": "xgvWZ7AjP", "sala-1-3": "", "sala-1-4": "", "sala-1-5": "",
+    "sala-2-1": "", "sala-2-2": "", "sala-2-3": "", "sala-2-4": "", "sala-2-5": "",
+    "sala-3-1": "", "sala-3-2": "", "sala-3-3": "",
+    "intro": ""
+  },
+  en: {
+    "sala-1-1": "", "sala-1-2": "CucKJOxkC", "sala-1-3": "", "sala-1-4": "", "sala-1-5": "",
+    "sala-2-1": "", "sala-2-2": "", "sala-2-3": "", "sala-2-4": "", "sala-2-5": "",
+    "sala-3-1": "", "sala-3-2": "", "sala-3-3": "",
+    "intro": ""
+  },
+  fr: {
+    "sala-1-1": "", "sala-1-2": "hEsxoWsiX", "sala-1-3": "", "sala-1-4": "", "sala-1-5": "",
+    "sala-2-1": "", "sala-2-2": "", "sala-2-3": "", "sala-2-4": "", "sala-2-5": "",
+    "sala-3-1": "", "sala-3-2": "", "sala-3-3": "",
+    "intro": ""
+  },
+  de: {
+    "sala-1-1": "", "sala-1-2": "KybiV3mUa", "sala-1-3": "", "sala-1-4": "", "sala-1-5": "",
+    "sala-2-1": "", "sala-2-2": "", "sala-2-3": "", "sala-2-4": "", "sala-2-5": "",
+    "sala-3-1": "", "sala-3-2": "", "sala-3-3": "",
+    "intro": ""
+  },
+}
--- frontend/src/static/js/pages/HomePage.tsx
+++ frontend/src/static/js/pages/HomePage.tsx
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react';
 import { LinksConsumer, SiteConsumer } from '../utils/contexts/';
 import { Page } from './Page';
 import { Flag } from '../components/flags/flag';
-import { useLayout, useTheme } from '../utils/hooks';
+import { useTheme } from '../utils/hooks';
 import { Logo } from '../components/page-layout/PageHeader/Logo';
+import { PlaylistIds, VideoIds } from '../fixtures/video-map';
 
 const EmptyMedia: React.FC = ({ }) => {
   const { logo } = useTheme();
@@ -37,6 +38,16 @@ export const HomePage: React.FC<HomePageProps> = ({
   id = 'home'
 }) => {
 
+  const searchParams = new URLSearchParams(document.location.search)
+  const baseParam = searchParams.get('base');
+  const keys = Object.keys(VideoIds.it);
+  type VideoKey = keyof typeof VideoIds.it;
+
+  const baseUrlIt = baseParam && Object.prototype.hasOwnProperty.call(VideoIds.it, baseParam) ? "/view?m=" + VideoIds.it[baseParam as VideoKey] + "&pl=" : '/playlist/';
+  const baseUrlEn = baseParam && Object.prototype.hasOwnProperty.call(VideoIds.en, baseParam) ? "/view?m=" + VideoIds.en[baseParam as VideoKey] + "&pl=" : '/playlist/';
+  const baseUrlFr = baseParam && Object.prototype.hasOwnProperty.call(VideoIds.fr, baseParam) ? "/view?m=" + VideoIds.fr[baseParam as VideoKey] + "&pl=" : '/playlist/';
+  const baseUrlDe = baseParam && Object.prototype.hasOwnProperty.call(VideoIds.de, baseParam) ? "/view?m=" + VideoIds.de[baseParam as VideoKey] + "&pl=" : '/playlist/';
+
   return (
     <Page id={id}>
       <LinksConsumer>
@@ -44,10 +55,10 @@ export const HomePage: React.FC<HomePageProps> = ({
           <div className="language-selection-wrapper">
             <h1>Seleziona la lingua</h1>
             <div className="flags">
-              <Flag src="./static/images/flags/it.svg" alt={"IT"} title="Italian" href={"/playlists/z6l4XwVRZ"}></Flag>
-              <Flag src="./static/images/flags/en.svg" alt={"EN"} title="Italian" href={"/playlists/l6ny9U9Nc"}></Flag>
-              <Flag src="./static/images/flags/fr.svg" alt={"FR"} title="Italian" href={"/playlists/VcA9cZmA7"}></Flag>
-              <Flag src="./static/images/flags/de.svg" alt={"DE"} title="Italian" href={"/playlists/GAdwW1mSD"}></Flag>
+              <Flag src="./static/images/flags/it.svg" alt={"IT"} title="Italian" href={baseUrlIt + PlaylistIds.it}></Flag>
+              <Flag src="./static/images/flags/en.svg" alt={"EN"} title="English" href={baseUrlEn + PlaylistIds.en}></Flag>
+              <Flag src="./static/images/flags/fr.svg" alt={"FR"} title="Français" href={baseUrlFr + PlaylistIds.fr}></Flag>
+              <Flag src="./static/images/flags/de.svg" alt={"DE"} title="Deutsch" href={baseUrlDe + PlaylistIds.de}></Flag>
             </div>
           </div>
         )}
